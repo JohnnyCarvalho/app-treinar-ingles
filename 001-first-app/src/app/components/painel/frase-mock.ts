f11f15a7ad14bea99652b6e714b8a341ea46b343
Johnny Carvalho <johnnycarvalhoem@gmail.com>
2022-10-14 10:11:16 -0300

    commit

--- 001-first-app/src/app/components/painel/frase-mock.ts
+++ 001-first-app/src/app/components/painel/frase-mock.ts
@@ -5,5 +5,11 @@ export const FRASES: Array<Frase> = [
     {fraseEng: 'What is your name?', frasePtBr: 'Qual é seu nome?'},
     {fraseEng: 'How are you today?', frasePtBr: 'Como você está hoje?'},
     {fraseEng: 'I like to develop systems!', frasePtBr: 'Eu gosto de desenvolver sistemas!'},
-    {fraseEng: 'I like coffe!', frasePtBr: 'Eu gosto de café!'}
+    {fraseEng: 'I like coffe!', frasePtBr: 'Eu gosto de café!'},
+    
+    {fraseEng: 'I am a human', frasePtBr: 'Eu sou um humano'},
+    {fraseEng: 'Do you have pen?', frasePtBr: 'Vocie tem cabeta?'},
+    {fraseEng: 'What times it?', frasePtBr: 'Que horas são?'},
+    {fraseEng: 'I have a car and your color is red!', frasePtBr: 'Eu tenho um carro e sua cor é vermelho'},
+
 ]
